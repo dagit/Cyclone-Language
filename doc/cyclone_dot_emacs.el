ec9c51d4796125ec83cece034b8bb5cdd908114d
danieljg <danieljg@581db07f-6e62-4d40-9733-d432c4eebb07>
2001-02-19 23:17:21 +0000

    As you've seen in this space before, I hack c++-mode to do some Cyclone-like things.  Of course, the right thing to do would be to **copy** the c++-mode and rename it Cyclone mode, but since I don't hack in C++...
    Anyway, I've added two .el files to the doc directory.
    cyclone_dot_emacs.el should get integrated into your .emacs.  The
    other is a replacement for font-lock.el.  The idea is to put it
    earlier in your load-path than the system font-lock.el.  Also, I didn't
    provide a Makefile, so you should compile this file (to a .elc)
    manually.
    
    What's new: For patterns (between let ... = and between case ... :,
    everything is in "constant" color, except for
    identifiers-starting-with-lower-case which are in "variable" color.  So
    if, like our code, you use lower-case for locals and upper-case for
    variants, you get your buffer telling you what's being bound.
    I even got where-patterns right -- && after a case "stops the action"
    just like a :.
    
    At first I used a different heuristic -- all identifiers were variables
    unless they were followed by a ( or {.  This doesn't work well for
    zero-ary constructors, though.
    
    KNOWN BUG: When null appears in a pattern, the colors don't come out
    right.  This is an ordering thing I'll track down.
    
    
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@939 581db07f-6e62-4d40-9733-d432c4eebb07

---
+++ doc/cyclone_dot_emacs.el
@@ -0,0 +1,44 @@
+;; POPCORN AND CYCLONE
+(defun my-c++-hook ()
+  (modify-syntax-entry ?\` "w" c++-mode-syntax-table))
+(setq auto-mode-alist
+      (cons '("\\.h$"       . c++-mode) 
+      (cons '("\\.pop[l]?$" . c++-mode) 
+      (cons '("\\.cyl"      . c++-mode) 
+      (cons '("\\.cyc"      . c++-mode)
+      (cons '("\\.y"        . c++-mode)
+      auto-mode-alist))))))
+(font-lock-add-keywords 'c++-mode ; reserved words not in C++ and not types
+    (list 
+      "\\\<xenum\\\>" 
+      "\\\<fallthru\\\>"
+      "\\\<default\\\>"
+      "\\\<abstract\\\>"
+      "\\\<codegen\\\>"
+      "\\\<splice\\\>"
+      "\\\<cut\\\>"
+      "\\\<fill\\\>"
+      "\\\<where\\\>"
+      '("\\\<null\\\>" . 'font-lock-constant-face)
+      ; color the pattern like a constant, except identifiers starting
+      ; with lower-case are variables.
+      '("\\(\\\<let\\\>\\)\\([^=\n]*\\)=" 
+	 (1 'font-lock-type-face) 
+	 (2 'font-lock-constant-face)
+	 ("[^A-Za-z0-9_]\\([a-z][A-Za-z0-9_]*\\)"  
+	  (cond (t (goto-char (match-end 1)) (match-end 2)))
+	  nil
+	  (1 font-lock-variable-name-face t t)))
+      '("\\\<let\\\>" . 'font-lock-type-face)))
+
+(setq c++-font-lock-extra-types 
+   '(; tyvars start with `
+     "`\\sw+" 
+     ; not really a type, but this gets the non-pattern (add _ for Popcorn)
+     "_"
+     ; C convention -- use _t to end your typedefs
+     "\\sw+_t" 
+     ; library types -- feel free to add more
+     "FILE" "string" "list" "Set" "Dict"
+     ))
+(setq c++-mode-hook 'my-c++-hook)
