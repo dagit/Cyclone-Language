3dd84b5300955c66fa2eedfadc5fd0752795cf6e
jcheney <jcheney@581db07f-6e62-4d40-9733-d432c4eebb07>
2001-05-14 19:57:09 +0000

    Fixed the ocamllex exceptions so now they work again. Added the lexer.h file that I forgot to add before.  Modified markov.cyc so that everything is heap-allocated, in order to see if it runs; there's some problem with how the resulting version gets translated that Dan says he'll fix.
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@1031 581db07f-6e62-4d40-9733-d432c4eebb07

---
+++ tools/cyclex/lexer.h
@@ -0,0 +1,26 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include "core.h"
+#include "stdio.h"
+#include "string.h"
+#include "lexing.h"
+
+#include "syntax.h"
+#include "parser.h"
+
+using Core {
+using Stdio {
+using Lexing {
+using Parser {
+namespace Lexer {
+
+extern xtunion exn { Lexical_error(string,int,int); };
+
+extern int lexmain(Lexing::Lexbuf<`a>);
+extern int line_num;
+extern int line_start_pos;
+} // namespace Lexer
+}}}}
+
+#endif
