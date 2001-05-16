df3f7b30fd7807d9359affc9a09961f541c5e976
jcheney <jcheney@581db07f-6e62-4d40-9733-d432c4eebb07>
2001-05-16 18:13:36 +0000

    Ported a couple of data compression algorithms, and put some more gripes in the porting.txt file.
    compress/cacm contains the Cyclone version of arithmetic coding published in CAC
    M in 1984.  Remarkably, this works just fine.
    
    compress/ppm contains Teahan's implementation of the PPM data compression algori
    thm.  All the files except model.cyc compile.
    
    Model.cyc contains several large arrays (on the order of about a megabyte each).
      It turns out that doing
    
    #define FILE_SIZE 13000000
    ...
    static unsigned char Input [FILE_SIZE] = {for i < FILE_SIZE:0};
    
    is a good way to find out what happens when you use up all your swap space, sinc
    e Cyclone apparently responds to this challenge by constructing a very large abs
    tract syntax tree containing many zeros.  Or at least trying to.
    
    I can code around this but I thought I'd share it with you in its purest, most p
    ainful form first.
    
    
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@1038 581db07f-6e62-4d40-9733-d432c4eebb07

---
+++ tests/compress/ppm/arithcode.h
@@ -0,0 +1,41 @@
+/*
+	Declarations for arithmetic coding.
+	Alistair Moffat, alistair@cs.mu.oz.au, July 1987.
+
+	@(#)arithcode.h	1.1 5/3/91
+*/
+
+#define codevaluebits 16
+#if (codevaluebits<16)
+typedef unsigned short codevalue;
+#else
+typedef unsigned long  codevalue;
+#endif
+
+#define topvalue ((codevalue)((1<<codevaluebits)-1))
+#define maxfrequency ((topvalue+1)/4 + 1)
+
+#define firstqtr (topvalue/4+1)
+#define half     (2*firstqtr)
+#define thirdqtr (3*firstqtr)
+
+#define	escape_event	U->totalcnt-U->notfound, U->totalcnt, U->totalcnt
+
+void arithmetic_encode(int lbnd, int hbnd, int total);
+/* codevalue  arithmetic_decode_target(); */
+#define	arithmetic_decode_target(totl)	\
+	(  ((S_value-S_low+1)*totl-1) / (S_high-S_low+1)  )
+void arithmetic_decode(int lbnd, int hbnd, int total);
+
+void startencoding();
+void startoutputingbits();
+void doneoutputingbits();
+void doneencoding();
+void startinputingbits();
+void startdecoding();
+
+codevalue	S_low, S_high, S_value;
+long		S_bitstofollow;
+int		S_buffer, S_bitstogo;
+
+long		cmpbytes, rawbytes;
