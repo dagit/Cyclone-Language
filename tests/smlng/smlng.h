3e65cddd47c44134d11aa4cfb0b3a72c7ea70fb9
jgm <jgm@581db07f-6e62-4d40-9733-d432c4eebb07>
2001-07-26 18:14:33 +0000

    Initial stab at the programming contest stuff.
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@1203 581db07f-6e62-4d40-9733-d432c4eebb07

---
+++ tests/smlng/smlng.h
@@ -0,0 +1,15 @@
+#ifndef _SMLNG_H_
+#define _SMLNG_H_
+extern enum Tag { Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine,
+           B_tag, EM_tag, I_tag, PL_tag, S_tag, TT_tag, U_tag, r_tag,
+           g_tag, b_tag, c_tag, m_tag, y_tag, k_tag, w_tag
+};
+typedef enum Tag tag_t;
+extern tunion Document;
+typedef tunion Document doc_t;
+extern tunion Document {
+  Seq(doc_t ?);
+  Text(char ?);
+  Tagged(tag_t,doc_t);
+};
+#endif _SMLNG_H_
