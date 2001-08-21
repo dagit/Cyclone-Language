a0ce6c511707f0e1c871f69daa2c6d38e141a249
danieljg <danieljg@581db07f-6e62-4d40-9733-d432c4eebb07>
2001-08-21 17:18:58 +0000

    Removed contents of bin/genfiles/lib and bin/genfiles/src -- these have been replaced by the architecture-specific directories
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@1348 581db07f-6e62-4d40-9733-d432c4eebb07

--- bin/genfiles/lib/precore_c.h
+++
@@ -1,8 +0,0 @@
-typedef unsigned char *Cstring;
-typedef struct _tagged_arr string_t;
-typedef struct _tagged_arr mstring_t;
-typedef string_t *stringptr_t;
-typedef mstring_t *mstringptr_t;
-typedef int bool;
-extern void exit(int);
-extern void *abort();
