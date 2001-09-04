0e45913ae3ff1a1cf8f6f6c6b3dd17196019da0c
tjim <tjim@581db07f-6e62-4d40-9733-d432c4eebb07>
2001-09-04 21:26:13 +0000

    First library documented with cycdoc: xarray.
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@1407 581db07f-6e62-4d40-9733-d432c4eebb07

--- include/xarray.h
+++ include/xarray.h
@@ -27,58 +27,82 @@
 #include <core.h>
 namespace Xarray {
 
+  /*** \subsection{\texttt{<xarray.h>}} */
+  /*** Defines namespace Xarray, which implements a datatype of
+       extensible arrays. */
+
 using Core;
 
 struct Xarray<`a::B> {
   `a ?elmts;
   int num_elmts;
 };
+
 typedef struct Xarray<`a> @xarray_t<`a>;
-
+  /** An [xarray_t] is an extensible array. */
 extern int length(xarray_t<`a>);
-
+  /** [length(a)] returns the length of extensible array [a]. */
 extern `a get(xarray_t<`a>, int);
-
+  /** [get(a,n)] returns the [n]th element of [a], or throws
+      [Invalid_argument] if [n] is out of range. */
 extern void set(xarray_t<`a>, int, `a);
-
+  /** [set(a,n,v)] sets the [n]th element of [a] to [v], or throws
+      [Invalid_argument] if [n] is out of range. */
 extern xarray_t<`a> create(int, `a);
-
+  /** [create(n,v)] returns a new extensible array with starting size [n]
+      and default value [v]. */
 extern xarray_t<`a> create_empty();
-
+  /** [create_empty()] returns a new extensible array with starting
+      size 0. */
 extern xarray_t<`a> singleton(int, `a);
-
+  /** [singleton(n,v)] returns a new extensible array with a single
+      element [v]. */
 extern void add(xarray_t<`a>, `a);
-
+  /** [add(a,v)] makes the extensible array larger by adding [v] to
+      the end. */
 extern int add_ind(xarray_t<`a>, `a);
-
+  /** [add_ind(a,v)] makes [a] larger by adding [v] to the end, and
+      returns [v]. */
 extern `a ?to_array(xarray_t<`a>);
-
+  /** [to_array(a)] returns a normal (non-extensible) array with the
+      same elements as [a]. */
 extern xarray_t<`a> from_array(`a ?arr);
-
-extern xarray_t<`a> append(xarray_t<`a>, xarray_t<`a>); // functional
-
+  /** [from_array(a)] returns an extensible array with the same
+      elements as the normal (non-extensible) array [a]. */
+extern xarray_t<`a> append(xarray_t<`a>, xarray_t<`a>);
+  /** [append(a1,a2)] returns a new extensible array whose elements
+      are the elements of [a1] followed by [a2].  The inputs [a1] and
+      [a2] are not modified. */
 extern void imp_append(xarray_t<`a>, xarray_t<`a>); // imperative
-
+  /** [imp_append(a1,a2)] DOES NOT EXIST! */
 extern void app(`b f(`a), xarray_t<`a>);
-
+  /** [app(f,a)] applies [f] to each element of [a], in order
+      from lowest to highest.  Note that [f] returns [`a], unlike
+      with [iter]. */
 extern void app_c(`b f(`c,`a), `c, xarray_t<`a>);
-
+  /** [app_c(f,e,a)] applies [f] to [e] and each element of [a], in
+      order from lowest to highest. */
 extern void iter(void f(`a), xarray_t<`a>);
-
+  /** [iter(f,a)] applies [f] to each element of [a], in order
+      from lowest to highest.  Note that [f] returns [void], unlike
+      with [app]. */
 extern void iter_c(void f(`b,`a), `b, xarray_t<`a>);
-
+  /** [iter_c(f,e,a)] applies [f] to [e] and each element of [a], in
+      order from lowest to highest. */
 extern xarray_t<`b> map(`b f(`a), xarray_t<`a>);
-
+  /** [map(f,a)] returns a new extensible array whose elements are
+      obtained by applying [f] to each element of [a]. */
 extern xarray_t<`b> map_c(`b f(`c,`a), `c, xarray_t<`a>);
-
+  /** [map_c(f,e,a)] returns a new extensible array whose elements are 
+      obtained by applying [f] to [e] and each element of [a]. */
 extern void reuse(xarray_t<`a> xarr);
-
-// Delete the last num elements.
+  /** [reuse(a)] sets the number of elements of [a] to zero, but
+      does not free the underlying array. */
 extern void delete(xarray_t<`a> xarr, int num);
-
-// Remove the element at position i.
-// Slide all higher elements down one position.
+  /** [delete(a,n)] deletes the last [n] elements of [a]. */
 extern void remove(xarray_t<`a> xarr, int i); 
+  /** [remove(a,i)] removes the element at position [i] from [a];
+      all higher elements are moved down one position. */
 
 }
 
