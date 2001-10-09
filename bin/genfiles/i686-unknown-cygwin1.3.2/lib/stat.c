990350b1e79991efa4f2beff2ad3c2afca9eb0ff
tjim <tjim@581db07f-6e62-4d40-9733-d432c4eebb07>
2001-10-09 17:56:54 +0000

    Preliminary version of a tool to build interfaces to C code.
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@1475 581db07f-6e62-4d40-9733-d432c4eebb07

--- bin/genfiles/i686-unknown-cygwin1.3.2/lib/stat.c
+++ bin/genfiles/i686-unknown-cygwin1.3.2/lib/stat.c
@@ -236,26 +236,15 @@ const int* timep); extern int timezone; extern int daylight; struct Cyc_Std_stat
 short st_dev; unsigned int st_ino; int st_mode; unsigned short st_nlink;
 unsigned short st_uid; unsigned short st_gid; short st_rdev; int st_size;
 unsigned int st_blksize; unsigned int st_blocks; int st_atime; int st_mtime; int
-st_ctime; } ; extern int Cyc_Std_open( struct _tagged_arr pathname, int flags,
-struct _tagged_arr mode); extern int Cyc_Std_stat( struct _tagged_arr filename,
-struct Cyc_Std_stat_t* buf); extern int fstat( int fd, struct Cyc_Std_stat_t*
-buf); extern int Cyc_Std_lstat( struct _tagged_arr filename, struct Cyc_Std_stat_t*
-buf); extern int umask( int mask); extern int Cyc_Std_mkdir( struct _tagged_arr
-pathname, int mode); extern int open( unsigned char* pathname, int flags, int
-mode); extern int stat( unsigned char* filename, struct Cyc_Std_stat_t* buf);
-extern int fstat( int fd, struct Cyc_Std_stat_t* buf); extern int lstat(
-unsigned char* filename, struct Cyc_Std_stat_t* buf); extern int mkdir(
-unsigned char* pathname, int mode); int Cyc_Std_open( struct _tagged_arr
-pathname, int flags, struct _tagged_arr mode){ int my_mode; if( _get_arr_size(
-mode, sizeof( int)) ==  0){ my_mode= - 1;} else{ if( _get_arr_size( mode,
-sizeof( int)) ==  1){ my_mode=*(( int*) _check_unknown_subscript( mode, sizeof(
-int), 0));} else{( int) _throw(( void*)({ struct Cyc_Core_Failure_struct* _temp0=(
-struct Cyc_Core_Failure_struct*) _cycalloc( sizeof( struct Cyc_Core_Failure_struct));
-_temp0[ 0]=({ struct Cyc_Core_Failure_struct _temp1; _temp1.tag= Cyc_Core_Failure;
-_temp1.f1= _tag_arr("open: too many args", sizeof( unsigned char), 20u); _temp1;});
-_temp0;}));}} return open( string_to_Cstring( pathname), flags, my_mode);} int
-Cyc_Std_stat( struct _tagged_arr filename, struct Cyc_Std_stat_t* buf){ return
-stat( string_to_Cstring( filename), buf);} int Cyc_Std_lstat( struct _tagged_arr
-filename, struct Cyc_Std_stat_t* buf){ return lstat( string_to_Cstring( filename),
-buf);} int Cyc_Std_mkdir( struct _tagged_arr pathname, int mode){ return mkdir(
-string_to_Cstring( pathname), mode);}
+st_ctime; } ; extern int Cyc_Std_stat( struct _tagged_arr filename, struct Cyc_Std_stat_t*
+buf); extern int fstat( int fd, struct Cyc_Std_stat_t* buf); extern int Cyc_Std_lstat(
+struct _tagged_arr filename, struct Cyc_Std_stat_t* buf); extern int umask( int
+mask); extern int Cyc_Std_mkdir( struct _tagged_arr pathname, int mode); extern
+int stat( unsigned char* filename, struct Cyc_Std_stat_t* buf); extern int fstat(
+int fd, struct Cyc_Std_stat_t* buf); extern int lstat( unsigned char* filename,
+struct Cyc_Std_stat_t* buf); extern int mkdir( unsigned char* pathname, int mode);
+int Cyc_Std_stat( struct _tagged_arr filename, struct Cyc_Std_stat_t* buf){
+return stat( string_to_Cstring( filename), buf);} int Cyc_Std_lstat( struct
+_tagged_arr filename, struct Cyc_Std_stat_t* buf){ return lstat(
+string_to_Cstring( filename), buf);} int Cyc_Std_mkdir( struct _tagged_arr
+pathname, int mode){ return mkdir( string_to_Cstring( pathname), mode);}
