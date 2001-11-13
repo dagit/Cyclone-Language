1c49caaac2d4ac1c0d00c8d1aa7ab8a25f9f4c74
mhicks <mhicks@581db07f-6e62-4d40-9733-d432c4eebb07>
2001-11-13 22:22:40 +0000

    Set up shootout Makefile to create various versions of the test for runtime comparison.  Added two scripts, test.sh and runtests, that run the tests.  test.sh does a single test [n] times, and runtests invokes this script for each test.  The results are dumped to stdout, one line per test.  I'm going to write another script that munges over this output, takes the median, and generates the table rows that we're interested in.
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@1627 581db07f-6e62-4d40-9733-d432c4eebb07

---
+++ tests/shootout/test.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# usage: test.sh n prog [inputfile]
+#   where n is the number of times to run each file
+#         prog1 is the program to run (including its args)
+#         inputfile is the optional input file to redirect to stdin
+
+TIME=/usr/bin/time
+TMP=/tmp/test$$
+
+# Args
+
+if [ $# -lt 2 ]; then
+cat<<EOF
+usage: $0 n prog [inputfile]
+  where n is the number of times to run each file
+        prog1 is the program to run (including its args)
+        inputfile is the optional input file to redirect to stdin
+EOF
+exit 1
+
+else
+  N=$1
+  CMD=$2
+  if [ $# -ge 3 ]; then
+    INPUTFILE=$3
+  fi
+fi
+
+# Run it
+
+NAME=`echo $CMD | awk '{ print $1; }'`
+NAME=${NAME%.exe}
+echo -n "${NAME#./} "
+
+i=0
+rm -f $TMP
+while [ "$i" != "$N" ]; do
+  if [ -n "$INPUTFILE" ]; then
+    $TIME --format="%e" $CMD 2>> $TMP 1> /dev/null <$INPUTFILE
+  else
+    $TIME --format="%e" $CMD 2>> $TMP 1> /dev/null
+  fi
+  i=`expr $i + 1`
+done
+cat $TMP | awk '{ printf("%s ",$1); }'
+echo
+rm -f $TMP
