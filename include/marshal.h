58d1d73a7360def0e01c27a2e35bc355934a0ff5
jcheney <jcheney@581db07f-6e62-4d40-9733-d432c4eebb07>
2002-01-21 17:31:25 +0000

    Added a chapter (very preliminary) for talking about generic/polytypic functions, and documented marshal.  Fixed a couple of bugs in doc/ that kept the documentation from building.
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@1870 581db07f-6e62-4d40-9733-d432c4eebb07

--- include/marshal.h
+++ include/marshal.h
@@ -26,19 +26,60 @@
 #include <typerep.h>
 
 namespace Marshal {
-
+  /** In all of the following functions, the representation [rep] passed 
+      to a function must be a representation of the type `a at 
+      which the function is instantiated.  You can use the [__gen(t)]
+      primitive to construct representations for types automatically.
+      (This gets trickier when type definitions cross module 
+      boundaries).
+  */
 
 extern void fprint_type(tunion Typerep::Typestruct rep, FILE@ fp,`a::A@ val);
+  /** [fprint_type(rep,fp,val)] takes a representation of type [`a], 
+      an open file pointer [fp], and [val], a pointer to [`a], and 
+      (ugly-)prints a text representation of [*val] to [fp].
+  */
 extern void print_type(tunion Typerep::Typestruct rep, `a::A@ val);
-
+  /** [print_type(rep,val)] is [fprint_type] directed to [stdout]. 
+   */
 extern void write_type(tunion Typerep::Typestruct rep, FILE@ fp, `a::A@ val);
+  /** [write_type(rep,fp,val)] writes a binary representation out to [fp].
+      ([fp] should be in binary mode on operating systems where there is a
+      difference).
+  */
 extern `a::A@`r rread_type(region_t<`r> r,tunion Typerep::Typestruct rep, FILE@ fp);
+  /** [rread_type(r,rep,fp)] reads a value encoded in the binary format of 
+      [write_type] from [fp], allocating into region [`r], and returns 
+      a pointer to the value. 
+      Warning: Currently, xtunions values cannot be safely read.
+  */
 extern `a::A@ read_type(tunion Typerep::Typestruct rep, FILE@ fp);
+  /** [read_type(rep,fp)] is [rread_type(heap_region,rep,fp)].
+   */
 extern `a::A@`r rcopy_type(region_t<`r>,tunion Typerep::Typestruct rep, `a@ val);
+  /** [rcopy_type(r,rep,val)] makes an exact structural (deep) copy of 
+      [*val], allocating into region [`r].
+  */
 extern `a::A@ copy_type(tunion Typerep::Typestruct rep, `a@ val);
+  /** [copy_type(rep,val)] is [rcopy_type(rep,heap_region,val)].
+   */
 extern bool leq_type(tunion Typerep::Typestruct rep, `a::A@ x, `a@ y);
+  /** [leq_val(rep,x,y)] compares two values of the same type.
+   */
 extern bool eq_type(tunion Typerep::Typestruct rep, `a::A@ x, `a@ y);
+  /** [eq_type(rep,x,y)] tests structural equality of [x] and [y].
+      Attempts to handle cycles and sharing efficiently/safely.
+  */
 extern int cmp_type(tunion Typerep::Typestruct rep, `a::A@ x, `a@ y);
+  /** [cmp_type(rep,x,y)] compares [x] and [y], returning 0 if [x == y], 
+      1 if [x < y] and -1 if [x > y].  Can be used as a comparison function
+      for dictionary, set modules. 
+  */
+
+  // Further ideas:
+  // hash_type(rep,x)
+  // $(data,ptr,padding) size_type(rep,x)
+  // doc_t pp_type
 
 }
 
