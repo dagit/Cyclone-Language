817af94395810496dec46920e9f4b0c667ace043
tjim <tjim@581db07f-6e62-4d40-9733-d432c4eebb07>
2002-12-31 03:44:28 +0000

    The absynpp genfile was out of sync for some reason (setting of print_all_kinds in tc_params_r ?)
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@2454 581db07f-6e62-4d40-9733-d432c4eebb07

--- bin/genfiles/absynpp.c
+++ bin/genfiles/absynpp.c
@@ -1128,7 +1128,7 @@ fs->curr_namespace;}struct Cyc_Absynpp_Params Cyc_Absynpp_cyc_params_r={0,0,0,0,
 1,0,0,0,1,1,0,1,0,1,0};struct Cyc_Absynpp_Params Cyc_Absynpp_cyci_params_r={1,0,0,
 0,0,1,0,0,1,1,1,0,1,0,1,0};struct Cyc_Absynpp_Params Cyc_Absynpp_c_params_r={1,1,1,
 0,1,0,0,0,0,0,0,0,0,0,0,0};struct Cyc_Absynpp_Params Cyc_Absynpp_tc_params_r={0,0,
-0,0,0,0,0,1,0,1,1,0,1,0,0,0};static void Cyc_Absynpp_curr_namespace_add(struct
+0,0,0,0,0,0,0,1,1,0,1,0,0,0};static void Cyc_Absynpp_curr_namespace_add(struct
 _dyneither_ptr*v);static void Cyc_Absynpp_curr_namespace_add(struct _dyneither_ptr*
 v){struct Cyc_List_List*_tmp5E5;Cyc_Absynpp_curr_namespace=((struct Cyc_List_List*(*)(
 struct Cyc_List_List*x,struct Cyc_List_List*y))Cyc_List_imp_append)(Cyc_Absynpp_curr_namespace,((
