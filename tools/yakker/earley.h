7f96c2436832985686e34539ad30e928aff4c436
yitzhak <yitzhak@581db07f-6e62-4d40-9733-d432c4eebb07>
2007-11-01 12:34:11 +0000

    Added new hooks for client and server sessions in revised imap. Added option to earley shell parse command (--show-prog) to print estates progress. Otherwise, parsing is mostly silent.
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@5661 581db07f-6e62-4d40-9733-d432c4eebb07

--- tools/yakker/earley.h
+++ tools/yakker/earley.h
@@ -23,7 +23,7 @@
 
 namespace Earley {
   extern $(dfa_t,Set::set_t<st_t>) compile(grammar_t grm, rule_t r);
-  extern int recognize(dfa_t dfa, Set::set_t<st_t> dfa_final, const char ?input);
+  extern int recognize(dfa_t dfa, Set::set_t<st_t> dfa_final, const char ?input, int show_progress);
 
   /* Global variable that saves last derivation tree.
    !! Treat as read-only outside of earley.cyc.
