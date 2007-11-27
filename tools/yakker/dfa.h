05ec0f7e4333c0a23cd378558ab7762cbcbd18c1
yitzhak <yitzhak@581db07f-6e62-4d40-9733-d432c4eebb07>
2007-11-27 20:11:18 +0000

    Minor modfications
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@5721 581db07f-6e62-4d40-9733-d432c4eebb07

--- tools/yakker/dfa.h
+++ tools/yakker/dfa.h
@@ -3,12 +3,10 @@
 #include "fa.h"
 
 namespace DFA {
+	// What's needed for building new Earley parsing DFAs based on a 
+	// pre-exisiting grammar.
 	struct dfa_kit {
 	  dfa_t dfa;
-	  // start state is implicitly state 1.
-	  // Set of final states for the *whole* dfa. This is *not*
-	  // the set of all final states, including those of symbols, in the dfa.
-	  Set::set_t<st_t> final_states;
 
 	  unsigned int symb_action_counter;
 	  struct Hashtable::Table<str_t,act_t> @symb_action_table;
@@ -19,15 +17,18 @@ namespace DFA {
 	};
   
   typedef struct dfa_kit@ dfa_kit_t;
+
+  // What's needed for Earley parsing.
+  struct dfa_rep {
+	  dfa_t dfa;
+	  st_t start;
+	  struct Hashtable::Table<act_t,str_t> @action_symb_table;	  
+  };
   
   //////////
-  // Accessor functions (basic)
+  // Accessor functions for DFA kit.
   /////////
 
-  // Get the start state of the dfa itself.
-  extern st_t get_dfa_start(dfa_kit k);
-  // Get the set of final states for the dfa itself.
-  extern Set::set_t<st_t> get_dfa_finals(dfa_kit k);
   // Get the set of final states for all symbols in the dfa.
   extern Set::set_t<st_t> get_all_symbol_finals(dfa_kit k);
   
@@ -40,7 +41,7 @@ namespace DFA {
   
   // Create a new symbol-action for the symbol argument.
   extern act_t new_symact(dfa_kit k, const char ?symb);
-  // Get the action for the specified symbol.
+  // Get the symbol-action for the specified symbol.
   extern act_t lookup_symact(dfa_kit k, const char ?symb);
   // Map a symbol-action to a symbol.
   extern const char ?lookup_symbol(dfa_kit k, act_t act);
