25909bf47d503cdcb0f3d616c6def49b976c351b
yitzhak <yitzhak@581db07f-6e62-4d40-9733-d432c4eebb07>
2007-12-10 16:34:38 +0000

    Added code to generate unambigous parse tree, with simpler representation. Fixed crawler code to work correctly (almost).
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@5744 581db07f-6e62-4d40-9733-d432c4eebb07

--- tools/yakker/earley-backend.h
+++ tools/yakker/earley-backend.h
@@ -22,22 +22,31 @@ namespace EarleyBackend {
 //	extern struct DFA::Rep::dfa_rep *rule2dfa(rule_t r, DFA::Kit::dfa_kit_t dfa_kit);
 //	extern struct DFA::Rep::dfa_rep *pat2dfa(rule_pat_t p, DFA::Kit::dfa_kit_t dfa_kit);
 
-  typedef List::list_t<datatype ParseTree @`H,`H> parse_forest_t;
-  typedef List::List_t<datatype ParseTree @`H,`H> Parse_forest_t;
+  typedef List::list_t<datatype SharedParseTree @`H,`H> shared_parse_forest_t;
+  typedef List::List_t<datatype SharedParseTree @`H,`H> Shared_parse_forest_t;
 
-  typedef datatype ParseTree @`H parse_tree_t;
+  typedef datatype SharedParseTree @`H shared_parse_tree_t;
 
-  extern datatype ParseTree{
-    NonTerm(const char ?`H, int, int, Semiring::weight_t, parse_forest_t);
+  extern datatype SharedParseTree{
+    NonTerm(const char ?`H, int, int, Semiring::weight_t, shared_parse_forest_t);
     SharedNonTerm(const char ?`H, int, int, Semiring::weight_t, 
-    		List::List_t<parse_forest_t,`H>);
+    		List::List_t<shared_parse_forest_t,`H>);
 //		List::List_t<$(parse_forest_t,Semiring::weight_t) @`H,`H>);
   };
   
+  typedef struct parse_tree @parse_tree_t;
+  struct parse_tree {
+  	string_t nonterminal;
+  	int left;
+  	int right;
+  	Semiring::weight_t weight;
+  	parse_tree_t ? children;
+  };
 
 	extern $(List::list_t<$(parse_tree_t,Semiring::weight_t)@>,int,int)
 	parse(DFA_TY dfa_rep, const char? input);
-  extern void print_LT(datatype ParseTree @pt, int depth);
+  extern void print_LT(datatype SharedParseTree @pt, int depth);
+  extern void print_tree(parse_tree_t pt, int depth);
 }
 
 #endif /*EARLEYBACKEND_H_*/
