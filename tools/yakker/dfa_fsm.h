4d9dfc7b4fcd48556532bcd81e0eaf5aaf7cdf83
yitzhak <yitzhak@581db07f-6e62-4d40-9733-d432c4eebb07>
2007-12-20 13:58:56 +0000

    Files relevant for extensible dfa work.
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@5761 581db07f-6e62-4d40-9733-d432c4eebb07

---
+++ tools/yakker/dfa_fsm.h
@@ -0,0 +1,37 @@
+#ifndef DFA_FSM_H_
+#define DFA_FSM_H_
+
+#define DFA_TRANS(dfa,s,a) (target(dfa->d,s,a))
+#define DFA_TRANS_W(dfa,s,a) (target_w_weight(dfa->d,s,a))
+#define DFA_GET_REPEAT_ACT(dfa,s) *(first_action_after(dfa->d,s,callout_action()))
+#define DFA_GET_REPEATEE_ACT(dfa,s) *(first_action_after(dfa->d,s,0))
+#define DFA_IN_FINAL(dfa,nt,s) (Set::member(dfa_final_attrs(dfa->d,s),nt))
+#define DFA_IS_FINAL(dfa,s) (dfa_is_final(dfa->d,s))
+#define DFA_R_EXTEND(dfa,nt,nt_start,nt_final) \
+	construct_repeat_dfa(dfa->d,nt,repeat_decr_action(),nt_start,\
+					                  repeat_final_action(), nt_final)
+#define DFA_FINAL_WEIGHT(dfa,f) dfa_final_weight(dfa->d,f)
+static att_t ?`H wrap_final_attrs(dfa_t dfa,st_t s) {
+	try {
+		let fs_set = dfa_final_attrs(dfa,s);
+		if (Set::is_empty(fs_set))
+				return new {};
+		return List::to_array(Set::to_list(fs_set));
+	} catch
+	{
+		case &Core::Not_found:
+		return NULL;
+	}
+}
+// XXX: find more efficient way to get array.
+#define DFA_FINAL_ATTRS(dfa,f) wrap_final_attrs(dfa->d,f) 
+#define DFA_GET_START(dfa) dfa->start
+#define DFA_ACT2SYMB(dfa,a) act2symb(attr,dfa->action_symb_table);
+
+// Macros for grammar DFAs.
+
+#define GRM_DFA_GET_SYMB_ACTION(dfa,symb) grm_get_symb_action(dfa,symb)
+#define GRM_DFA_GET_SYMB_START(dfa,a) grm_get_symb_start(dfa,a)
+#define GRM_DFA_GET_NUM_STATES(dfa) grm_get_num_states(dfa)
+
+#endif /*DFA_FSM_H_*/
