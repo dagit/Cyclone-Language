9c5826bae9e3ef4b6ef7cc7b78faf403c04c2d66
trevor <trevor@581db07f-6e62-4d40-9733-d432c4eebb07>
2008-02-14 21:24:23 +0000

    Cleanup/commenting of flatten code.
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@5845 581db07f-6e62-4d40-9733-d432c4eebb07

--- tools/yakker/flatten.h
+++ tools/yakker/flatten.h
@@ -20,12 +20,12 @@
 #define FLATTEN_H
 #include <core.h>
 #include "bnf.h"
-extern grammar_t flatten_grammar(grammar_t<`H> g);
 
-namespace Flatten{
+namespace Flatten {
   // Flag: for alts, put every alternative as a separate symbol definition.
   // default is 0;
   extern int fully_flatten_alts;
   extern int is_flattened_symb(string_t s);
+  extern grammar_t flatten_grammar(grammar_t<`H> g);
 }
 #endif
