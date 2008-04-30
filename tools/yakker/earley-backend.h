4b7ebdb814d34c49823702e895de93430b17e167
yitzhak <yitzhak@581db07f-6e62-4d40-9733-d432c4eebb07>
2008-04-30 15:17:06 +0000

    Minor changes to control of earley graph printing.
    git-svn-id: https://source.seas.harvard.edu/svn/cyclone/trunk/cyclone@6095 581db07f-6e62-4d40-9733-d432c4eebb07

--- tools/yakker/earley-backend.h
+++ tools/yakker/earley-backend.h
@@ -8,16 +8,21 @@
 #include "ykbuf.h"
 
 namespace EarleyAnyBackend {
-extern $(List::list_t<$(EarleyParsetree::parse_tree_t,Semiring::weight_t)@>,int,int)
-parse(EarleyAnyBackend::DFA::edfa_t dfa_rep, const char? input);
 
-// If parse_until_eof is not enabled, this function will perform shortest-match.
-extern $(List::list_t<$(EarleyParsetree::parse_tree_t,Semiring::weight_t)@>,int,int)
-ykb_parse(EarleyAnyBackend::DFA::edfa_t dfa_rep, ykbuf_t@ ykb, int parse_until_eof);
-
-extern List::list_t<$(EarleyParsetree::spf_t, Semiring::weight_t) @> 
-ykb_parse_spf(EarleyAnyBackend::DFA::edfa_t dfa_rep, 
-              ykbuf_t@ ykb, int parse_until_eof);
+  // various parameters that control printing of earley graph
+  extern int print_earley_graph, eg_filt_lb, eg_filt_ub;
+  
+  
+  extern $(List::list_t<$(EarleyParsetree::parse_tree_t,Semiring::weight_t)@>,int,int)
+  parse(EarleyAnyBackend::DFA::edfa_t dfa_rep, const char? input);
+  
+  // If parse_until_eof is not enabled, this function will perform shortest-match.
+  extern $(List::list_t<$(EarleyParsetree::parse_tree_t,Semiring::weight_t)@>,int,int)
+  ykb_parse(EarleyAnyBackend::DFA::edfa_t dfa_rep, ykbuf_t@ ykb, int parse_until_eof);
+  
+  extern List::list_t<$(EarleyParsetree::spf_t, Semiring::weight_t) @> 
+  ykb_parse_spf(EarleyAnyBackend::DFA::edfa_t dfa_rep, 
+                ykbuf_t@ ykb, int parse_until_eof);
 }
 
 #endif /*EARLEYBACKEND_H_*/
